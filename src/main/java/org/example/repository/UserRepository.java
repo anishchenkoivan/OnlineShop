a9aeaaa9f7c6382e1d96d073ac6e090edc435d8b
Ivan Anishchenko <ivananisenko08@gmail.com>
2023-12-13 21:11:22 +0300

    Implement repositories

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package org.example.repository;
 
+import org.example.entity.User;
+
 public interface UserRepository {
+    UserId createUser();
+
+    void deleteUser();
+
+    void addToCart(User user, Products product, int amount);
+
+    void RemoveFromCart(User user, Products product, int amount);
 }

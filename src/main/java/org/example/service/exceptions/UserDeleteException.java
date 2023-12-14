7c4a55cb042cc8e9c34a10cca00b073895f7a1aa
Ivan Anishchenko <ivananisenko08@gmail.com>
2023-12-15 00:56:38 +0300

    Implement UserController

---
+++ src/main/java/org/example/service/exceptions/UserDeleteException.java
@@ -0,0 +1,2 @@
+package org.example.service.exceptions;public class UserDeleteException {
+}
--- src/main/java/org/example/service/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.service.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}

5f1b1d024b50b29255f61bf2e8a451ef2fe08594
DmitryDos <dostovalov.ds@phystech.edu>
2023-12-14 11:12:46 +0300

    Update Product and config for working with jsone

--- src/main/java/org/example/entity/Product.java
+++ src/main/java/org/example/entity/Product.java
@@ -1,10 +1,20 @@
 package org.example.entity;
 
 public enum Product {
-  WATER,
-  FLOUR,
-  BREAD,
-  VODKA,
-  DOSHIRAK,
-  PASTA;
+  WATER("water", 0),
+  FLOUR("flour", 0),
+  BREAD("bread", 0),
+  VODKA("vodka", 0),
+  COOKIE("cookie", 0),
+  WAFFLES("waffles", 0),
+  RICE("rice", 0),
+  PASTA("pasta", 0);
+
+  private String unit;
+  private final int value;
+
+  private Product(String unit, int value) {
+    this.unit = unit;
+    this.value = value;
+  }
 }
